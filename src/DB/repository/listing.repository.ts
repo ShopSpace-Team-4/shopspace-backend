a7220522945a9cfa5d00cf2a838fe0d66b61e3ba
ibrahim abdallah gad <167654378+ibrahimgad1@users.noreply.github.com>
2026-08-08 06:50:49 +0300

    intigrate WhatsApplink and Filter and sort and Pagination

--- src/DB/repository/listing.repository.ts
+++ src/DB/repository/listing.repository.ts
@@ -1,9 +1,24 @@
 import { BaseRepository } from './base.repository';
 import { ListingModel, IListing } from '../models/listing.model';
+import { FilterQuery } from 'mongoose';
 
 export class ListingRepository extends BaseRepository<IListing> {
   constructor() {
     super(ListingModel);
+  }
+
+  async findWithPagination(
+    filter: FilterQuery<IListing>,
+    sort: Record<string, 1 | -1>,
+    skip: number,
+    limit: number
+  ) {
+    const [items, total] = await Promise.all([
+      ListingModel.find(filter).sort(sort).skip(skip).limit(limit).exec(),
+      ListingModel.countDocuments(filter).exec(),
+    ]);
+
+    return { items, total };
   }
 }
 
