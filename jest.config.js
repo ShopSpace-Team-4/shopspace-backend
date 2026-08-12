c8f005e3087d56980cbb0cfd5cb87b8c948352e1
ibrahim abdallah gad <167654378+ibrahimgad1@users.noreply.github.com>
2026-08-12 06:42:40 +0300

    fix ai issue

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+      diagnostics: {
+        ignoreCodes: [151002],
+      },
+      },
+    ],
+  },
+};
